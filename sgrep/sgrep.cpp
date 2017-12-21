da9160ae9479f9177fc5db885a6af7a78139fde0
Maffyy <martin.studna2@gmail.com>
2017-12-21 23:36:03 +0100

    gerg

--- sgrep/sgrep.cpp
+++ sgrep/sgrep.cpp
@@ -2,17 +2,15 @@
 #include <string>
 #include <memory>
 #include <list>
-#include <sstream>
-#include <map>
 #include <set>
 #include <fstream>
 
 using namespace std;
 
+/* Token Interface */
 class token 
 {
 public:
-
 	virtual bool is_Char() {
 		return false;
 	}
@@ -46,12 +44,8 @@ public:
 	virtual char get_char() {
 		throw - 1;
 	}
-	virtual string get_var_name() {
-		throw - 1;
-	}
 };
-
-
+/*Creates token classes*/
 #define make_token(a) class t_##a : public token { \
 public:bool is_##a() { return true; }};
 
@@ -68,7 +62,7 @@ make_token(Or);
 class t_Char : public token
 {
 public:
-	t_Char(char& name) : name(name) {}
+	t_Char(const char& name) : name(name) {}
 
 	bool is_Char() {
 		return true;
@@ -85,17 +79,19 @@ toklist tokenize(const string& regexp)
 {
 	toklist t;
 	bool backslash = false;
-	for (char c : regexp) {
-		//cout << c << endl;
-
-		// TO-DO: Osetrit, pokud bude backslash na konci.
-		if (c == '\\') {
+	for (size_t i = 0; i < regexp.length(); ++i) {
+		/*If there is a backslash, move to next cycle and process next char*/
+		if (regexp[i] == '\\' && !backslash) {			
 			backslash = true;
 			continue;
-		} 
-		
-
-#define MATCH_TOKEN(s,l) if (c == s && !backslash) { t.push_back(make_unique<l>()); }
+		}
+		if (backslash) {
+			t.push_back(make_unique<t_Char>(regexp[i]));
+			backslash = false;
+			continue;
+		}
+/*Parse tokens to Token list*/
+#define MATCH_TOKEN(s,l) if (regexp[i] == s ) { t.push_back(make_unique<l>()); }
 		
 		MATCH_TOKEN('[', t_BracketOpen)
 		else MATCH_TOKEN(']', t_BracketClose)
@@ -107,40 +103,40 @@ toklist tokenize(const string& regexp)
 		else MATCH_TOKEN('.', t_Dot)
 		else MATCH_TOKEN('|', t_Or)
 		else {
-		t.push_back(make_unique<t_Char>(c));
+		t.push_back(make_unique<t_Char>(regexp[i]));
 		}
-		if (c != '\\') { backslash = false; }
+		
 #undef MATCH_TOKEN
-
 	}
 	return t;
 }
-/*TO-DO: vratit specialni hodnotu, pokud funkce neuspeje u nekterych regexu*/
 
 class Regex  {
 	size_t num;
-
 public:
 	virtual size_t match_part(const string& str, size_t begin, size_t max_len) = 0;
-
 	virtual bool contain_ParenRegex() {
 		return false;
 	}
-	// TO-DO: pokud nematchuju star, tak mi to vyhodi nejakou jinou hodnotu
 	const size_t npos = -1;
 };
 
 
 class OrRegex : public Regex {
 	unique_ptr<Regex> seq;
-	unique_ptr<Regex> or ;
+	unique_ptr<Regex> Or ;
 public:
-	OrRegex(unique_ptr<Regex> &&seq, unique_ptr<Regex> && or ) : seq(move(seq)), or (move(or )) {};
-
+	OrRegex(unique_ptr<Regex> &&seq, unique_ptr<Regex> &&Or ) : seq(move(seq)), Or(move(Or)) {};
+	bool contain_ParenRegex() {
+		if (seq->contain_ParenRegex() || seq->contain_ParenRegex()) {
+			return true;
+		}
+	}
 	size_t match_part(const string& str, size_t begin, size_t max_len) {
 		size_t left = seq->match_part(str, begin, max_len);
-		size_t right = or->match_part(str, begin, max_len);
-		if (left > right) { return left; }
+		size_t right = Or->match_part(str, begin, max_len);
+		if (left == npos && right == npos) { return npos; }
+		else if (left != npos && right == npos) { return left; }
 		else { return right; }
 	}
 };
@@ -148,25 +144,24 @@ class SeqRegex : public Regex {
 	unique_ptr<Regex> star, seq;
 public:
 	SeqRegex(unique_ptr<Regex> &&star, unique_ptr<Regex> seq) : star(move(star)), seq(move(seq)) {};
-	
-
-	size_t match_part(const string& str, size_t begin, size_t max_len) {
-		size_t matched = 0;
-		size_t m = 0;
-		/* jak se posouvat v seqregexu */
-
-		for (size_t i = begin; i < begin + max_len; i += m)
-		{
-			m = star->match_part(str, i, max_len);
-			//cout << m << endl;
-			if (m == 0) { i++;  }
-			matched += m;
+	bool contain_ParenRegex() {
+		if (star->contain_ParenRegex() || seq->contain_ParenRegex()) {
+			return true;
 		}
-
-		//matched = star->match_part(str, begin, max_len);
-		//cout << "true" << endl;
+	}
+	size_t match_part(const string& str, size_t begin, size_t max_len) {
+		if (star->contain_ParenRegex()) {
+			size_t matchedParen = star->match_part(str, begin, max_len);
+			return matchedParen;
+		}
+		size_t matched = star->match_part(str, begin, max_len);
+		if (matched == npos && (!star->contain_ParenRegex() || !seq->contain_ParenRegex())) { return npos; }
+		if (seq->contain_ParenRegex()) {
+			size_t matchedParen2 = seq->match_part(str, begin+matched, max_len);
+			return matchedParen2;
+		}
 		size_t matched2 = seq->match_part(str, begin + matched, max_len);
-	//	if (matched2 == 0) { return 0; }
+		if (matched2 == npos) { return npos; }
 		return matched + matched2;
 	}
 };
@@ -175,16 +170,18 @@ class StarRegex : public Regex {
 	unique_ptr<Regex> star;
 public:
 	StarRegex(unique_ptr<Regex> &&star) : star(move(star)) {};
-	// TO-DO: You have defined ParenRegex
-	//			Maybe you should implement it here.
+	bool contain_ParenRegex() {
+		if (star->contain_ParenRegex()) {
+			return true;
+		}
+	}
 	size_t match_part(const string& str, size_t begin, size_t max_len) {
 		size_t matched = 0;
 		size_t m =0;
-		for (size_t i = begin; i < begin+max_len; i+=m)
+		for (size_t i = begin; i < str.length(); i+=m)
 		{
 			m = star->match_part(str, i, max_len);
-			//cout << m << endl;
-			if (m == 0) { matched = 0; break; }
+			if (m == npos) { return matched; }
 			matched += m;
 		}
 		return matched;
@@ -193,64 +190,65 @@ public:
 class ParenRegex : public Regex {
 	unique_ptr<Regex> paren;
 public:
-
 	ParenRegex(unique_ptr<Regex> &&paren) : paren(move(paren)) {};
-
+	size_t match_part(const string& str, size_t begin, size_t max_len) {
+		size_t m = paren->match_part(str, begin, max_len);
+		if (m == npos) { return npos; }
+			cout << str.substr(begin) << endl;
+		return m;
+	}
 	bool contain_ParenRegex() {
 		return true;
 	}
-	size_t match_part(const string& str, size_t begin, size_t max_len) {
-		return paren->match_part(str, begin, max_len);
-	}
 };
-
 class Char : public Regex {
 	set<char> t;
 	bool caret;
 public:
 	Char(set<char>&& t, bool caret = false) : t(move(t)), caret(caret) {};
-
+	bool contain_ParenRegex() {
+		return false;
+	}
 	size_t match_part(const string& str, size_t begin, size_t max_len) {
+		if (begin > str.length() - 1) { return npos; }
 		if (t.count(str[begin]) && !caret) {
-		//	cout << "true" << endl;
 			return 1;
 		}
 		else if (!t.count(str[begin]) && caret) {
-			return 0;
+			return 1;
 		}
-		else return 0;
+		else return npos;
 	}
 };
-
-
 unique_ptr<Regex> parse_Regex(toklist& t);
-
 unique_ptr<Regex> parse_SimpleRegex(toklist& t) {
 
-	
+	/*Catch Char*/
 	if (!t.empty() && t.front()->is_Char()) {
 		set<char> s;
 		s.insert(t.front()->get_char());
 		t.pop_front();
 		return make_unique<Char>(move(s));
 	}
+	/*Catch Dot*/
 	if (!t.empty() && t.front()->is_Dot()) {
 		set<char> s;
 		for (size_t i = 0; i < 256; i++) { s.insert(i); }
 		t.pop_front();
 		return make_unique<Char>(move(s));
 	}
+	/*Catch Brackets*/
 	char c = 0;
 	if (!t.empty() && t.front()->is_BracketOpen()) {
 		set<char> s;
 		t.pop_front();
 		bool caret = false;
+		/*Catch all Chars in brackets*/
 		while (!t.front()->is_BracketClose()) {
 			if (t.front()->is_Caret()) { caret = true; }
 			if (t.front()->is_Char()) { 
 				c = t.front()->get_char();
 				s.insert(c);
-//				cout << "true" << endl;
 			}
 			if (t.front()->is_Hyphen()) {
 				t.pop_front();
@@ -262,38 +260,37 @@ unique_ptr<Regex> parse_SimpleRegex(toklist& t) {
 					}
 				}
 				else {
-					//cout << "true" << endl;
-					throw - 1;
+					throw "Missing EndChar for Hyphen";
 				}
 			}
 			t.pop_front();
 		}
 		if (!t.empty() && t.front()->is_BracketClose()) { t.pop_front(); }
 		else {
-		//	cout << "true" << endl;
-			throw - 3;
+			throw "BracketClose Missing";
 		}
 		if (caret) { return make_unique<Char>(move(s), true); }
 		else {
-			//cout << "true" << endl;
 			return make_unique<Char>(move(s)); }
 	}
-	// TO-DO: You have to find a way, how to make ParenRegex item.
+	/*Catch ParenRegex*/
 	if (!t.empty() && t.front()->is_ParenOpen()) {
 		t.pop_front();
 		auto regex = parse_Regex(t);
+		
 		if (!t.empty() && t.front()->is_ParenClose()) {
 			t.pop_front();
-			return make_unique<ParenRegex>(move(regex));
+			return  make_unique<ParenRegex>(move(regex));
 		}
 		else {
-			throw - 3;
+			throw "ParenClose Missing";
 		}
 	}
-	throw - 3;
+	if (!t.empty()) {
+		throw "Loading regular expression failed!";
+	}
+	throw "Undefined Error";
 }
-
-
 unique_ptr<Regex> parse_StarRegex(toklist& t) {
 	unique_ptr<Regex> simple = parse_SimpleRegex(t);
 	if (!t.empty() && t.front()->is_Star()) {
@@ -304,17 +301,15 @@ unique_ptr<Regex> parse_StarRegex(toklist& t) {
 		return simple;
 	}
 }
-
 unique_ptr<Regex> parse_SeqRegex(toklist& t) {
 	unique_ptr<Regex> star = parse_StarRegex(t);
-	if (!t.empty()) {
+	if (!t.empty() && !t.front()->is_Or() && !t.front()->is_ParenClose()) {
 		return make_unique<SeqRegex>(move(star), parse_SeqRegex(t));
 	}
 	else {
 		return star;
 	}
 }
-
 unique_ptr<Regex> parse_OrRegex(toklist& t) {
 	unique_ptr<Regex> seq = parse_SeqRegex(t);
 	if (!t.empty() && t.front()->is_Or()) {
@@ -325,43 +320,56 @@ unique_ptr<Regex> parse_OrRegex(toklist& t) {
 		return seq;
 	}
 }
-
 unique_ptr<Regex> parse_Regex(toklist& t) {
 	return parse_OrRegex(t);
 }
 
-//IDE geany
-
-int main(int argc, char ** argv) {
-
-	toklist t = tokenize("c");
-	
-	int x = 5;
-
+void processFile(toklist& t, ifstream& in) {
 	auto regex = parse_Regex(t);
-
-	cout << regex.get()->match_part("bca", 0, 3) << endl;
-	for(;;){}
+	const size_t npos = -1;
+	while (in.good() && !in.eof()) {
+		string line;
+		getline(in,line);
+		size_t res = -1;
+		if (res == line.length()) {
+			cout << line << endl;
+		}
+	}
+}
+void processInput(toklist& t) {
+	auto regex = parse_Regex(t);
+	const size_t npos = -1;
+	while (cin.good() && !cin.eof())
+	{
+		string line;
+		cin >> line;
+		size_t res = -1;
+	    res = regex->match_part(line, 0, line.length());
+			if (res == line.length() && !regex->contain_ParenRegex()) { 
+				cout << line << endl;
+			}
+	}
+}
+int main(int argc, char ** argv) {
+	string filename;
+	if (argc == 3) {
+		filename = argv[2];
+		toklist t = tokenize(argv[1]);
+		ifstream in(filename);
+		if (!in.good()) {
+			cerr << "file cannot be open" << endl;
+			return 1;
+		}
+		processFile(t, in);
+	}
+	else if (argc == 2) {
+		toklist t = tokenize(argv[1]);
+		processInput(t);
+	}
+	else {
+		cout << "Wrong parameters" << endl;
+		return 2;
+	}
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
